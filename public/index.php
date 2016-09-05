5ee0eacb46867338eed6c1ffc165b9a66a695206
Igor Goroshit <igor@webt.co.il>
2016-09-05 03:28:18 +0300

    fix trailing slashes

--- public/index.php
+++ public/index.php
@@ -2,28 +2,36 @@
 
 /*
 |--------------------------------------------------------------------------
+| Remove Trailing Slashes
+|--------------------------------------------------------------------------
+*/
+
+$uri    = $_SERVER['REQUEST_URI'];
+$len    = mb_strlen($uri);
+
+$query  = !empty($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : "";
+$sname  = mb_substr($_SERVER['SCRIPT_NAME'], 0, -9).$query;
+$rlen   = (mb_strlen($query) + 1);
+
+
+if($sname != $uri && $uri[$len - $rlen] === '/')
+{
+  $_SERVER['REQUEST_URI'] = mb_substr($uri, 0, -$rlen).$query;
+}  
+
+/*
+|--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
 */
 
 $app = require __DIR__.'/../bootstrap/app.php';
+
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
 */
-
 $request =  Illuminate\Http\Request::createFromGlobals();
 $app->run($request);
